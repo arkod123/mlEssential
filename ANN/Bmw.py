bfc87a8738cc6d01934634eba1a55873cc24a022
arkod123 <arko686@gmail.com>
2019-11-17 11:48:50 +0530

    second commit

--- ANN/Bmw.py
+++ ANN/Bmw.py
@@ -1,6 +1,13 @@
 
+# create a dummy class
 
 class Bmw:
     def __init__(self):
         self.models = ['i8', 'x1', 'x5', 'x6']
+
+    def OutModels(self):
+        for model in self.models:
+            print(model)
+
+
 
